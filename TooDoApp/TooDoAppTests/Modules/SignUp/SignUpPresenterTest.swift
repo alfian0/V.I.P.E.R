95afd80a19c9115026697b5466f914c49b348159
alfiansyah <twins.alfian@gmail.com>
2016-10-25 00:05:49 +0530

    Create test cases

--- TooDoApp/TooDoAppTests/Modules/SignUp/SignUpPresenterTest.swift
+++ TooDoApp/TooDoAppTests/Modules/SignUp/SignUpPresenterTest.swift
@@ -28,5 +28,31 @@ class SignUpPresenterTest: QuickSpec {
             presenter = SignUpPresenter(view: viewMock, viewModel: viewModel, interactor: interceptorMock, wireframe: wireFrameMock)
             interceptorMock.presenter = presenter
         }
+        
+        describe("Sign Up Test") {
+            it("Invalid Sign Up : Invalid Email Address", closure: {
+                presenter.doSignUp("", password: "", confirmPassword: "")
+                expect(viewMock.invocations.count) == 1
+                expect(viewMock.invocations).to(contain("showErrorMessage(Please enter a valid email)"))
+            })
+            
+            it("Invalid Sign Up : Invalid Password", closure: { 
+                presenter.doSignUp("alfiansyah@go-jek.com", password: "", confirmPassword: "")
+                expect(viewMock.invocations.count) == 1
+                expect(viewMock.invocations).to(contain("showErrorMessage(Please enter a valid password)"))
+            })
+            
+            it("Invalid Sign Up : Confirm password did not match", closure: {
+                presenter.doSignUp("alfiansyah@go-jek.com", password: "password", confirmPassword: "")
+                expect(viewMock.invocations.count) == 1
+                expect(viewMock.invocations).to(contain("showErrorMessage(Your password and confirm password did not match)"))
+            })
+            
+//            it("Invalid Sign Up : This email address is already in use", closure: {
+//                presenter.doSignUp("alfiansyah@go-jek.com", password: "password", confirmPassword: "password")
+//                expect(viewMock.invocations.count) == 1
+//                expect(viewMock.invocations).to(contain("showErrorMessage(This email address is already in use)"))
+//            })
+        }
     }
 }
